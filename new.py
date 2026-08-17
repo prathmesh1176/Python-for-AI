ad091f405c3754ef8ce8d817bdc233997829ad81
Prathmesh <prathmeshpatil1176@gmail.com>
2026-08-17 18:57:22 +0530

    Initial commit

---
+++ new.py
@@ -0,0 +1,29 @@
+a=2
+
+# a=a+2
+# a=a+2
+# a=a+2
+
+# print(a)
+
+# addition assignment operator
+
+# a+=2
+# a+=2
+# a+=2
+# print(a)
+
+# conditional statement
+# eg. make program for people who can vote who are above 18
+
+# age=int(input('Enter your age :'))
+
+# if (age<=18):
+#     print('you are not eligible to vote')
+# else:
+#     print('You are eligible to vote')
+
+
+
+
+# elif condition 
