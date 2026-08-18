87317d855a4f3d6ef9455e7e16d20c6b237fd435
Prathmesh <prathmeshpatil1176@gmail.com>
2026-08-18 10:39:49 +0530

    new commit

--- new.py
+++ new.py
@@ -27,3 +27,86 @@ a=2
 
 
 # elif condition 
+
+# money= int(input('Enter amount:-'))
+
+# if (money==10):
+#     print('Eat samosa')
+# elif(money==50):
+#     print('Eat Burger')
+# elif(money==100):
+#     print("buy toy")
+# else:
+#     print("sutti Babu")
+
+
+# questions
+
+# 1.Accept two numbers and print the greatest between them.
+# Input: 14, 7
+
+# a=int(input("1st no:-"))
+# b=int(input("2nd no:-"))
+
+# if (a>b):
+#     print(f"{a} is greater than {b}")
+# elif(a<b):
+#     print(f"{a} is less than {b}")
+# else:
+#     print("both numbers are same ")
+
+
+
+# Q2
+# Accept gender from user and print a greeting message.
+# Input: M
+# Output: Good Morning Sir
+
+# gender=input("Enter your Gender:-")
+# if (gender=="M" or gender=="m"):
+#     print('Hello Sir')
+# elif(gender=="F" or gender=="f"):
+#     print('Hello Mam')
+# else:
+#     print('others')
+
+
+# Q3
+# Accept an integer and check if it is even or odd.
+# Input: 9
+# Output: 9 is Odd
+
+# number=int(input("enter your value:-"))
+
+# if (number % 2==0):
+#     print("the number is even")
+# else:
+#     print("the number is odd")
+
+
+# Q4
+# Accept name and age — check if the user is a valid voter (18+).
+# Input: Shery, 20
+# Output: Hello Shery, you are a valid voter ✅
+
+# name= input("Enter your name:-")
+# age= int(input('Enter your age:-'))
+
+# if (age>=18):
+#     print(f"Hello {name}, you are eligible to vote")
+# else:
+#     print('Sorry you are not eligible') 
+
+
+# Q5
+# Accept a year and check if it is a leap year.
+# Input: 2024
+# Output: 2024 is a leap year ✅
+
+# year=int(input("enter year :"))
+# if year%100==0 and year%400==0:
+#     print('this is a leap year')
+# elif year%100!=0 and year%4==0:
+#     print("this is  a leap year")
+# else:
+#     print('this is not a leap year')
